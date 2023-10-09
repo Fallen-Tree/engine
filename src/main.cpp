1094a507869b03b150a94ab2caaf522e67ca0644
Matvey <matveymag@mail.ru>
2023-10-09 11:45:20 +0700

    Add camera

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,43 @@ static int SCR_WIDTH = 800;
 
 
 
+
+
+
+
+
+
+
 int main() {
     auto engine = Engine();
     engine.Run(SCR_WIDTH, SCR_HEIGHT);
+}
+
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn) {
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse) {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    camera.ProcessMouseMovement(xoffset, yoffset);
+}
+
+// glfw: whenever the mouse scroll wheel scrolls, this callback is called
+// ----------------------------------------------------------------------
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+    camera.ProcessMouseScroll(static_cast<float>(yoffset));
 }
 
 
