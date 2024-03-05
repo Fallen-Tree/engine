9421fd5308cd309fa85bd494c800264223e8b377
RomchikkF <r.fesenko@g.nsu.ru>
2024-03-05 22:18:25 +0700

    model path fix

--- src/model/model.cpp
+++ src/model/model.cpp
@@ -11,8 +11,10 @@
 
 Model* Model::loadFromFile(const char* path) {
     Model* newModel = new Model();
+    char finalPath[512];
+    snprintf(finalPath, sizeof(finalPath), RESOURCE_DIR"/models%s", path);
     Assimp::Importer import;
-    const aiScene *scene = import.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
+    const aiScene *scene = import.ReadFile(finalPath, aiProcess_Triangulate | aiProcess_FlipUVs);
     if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode) {
         Logger::Error("ERROR::ASSIMP::%s", import.GetErrorString());
         return 0;
