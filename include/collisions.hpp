733c67a6306c7a73825eabd9616fdfaae43ebc89
solloball <109233909+solloball@users.noreply.github.com>
2024-04-10 01:55:48 +0000

    OBB collision  (#66)
    * make obb
    
    * add empty test OBBvsAABB
    
    * make obb test
    
    * add obb to primitive
    
    * merge into master, resolve conflicts, add empty collision normal
    
    * make raycast, test with all type collider, remove logger
    
    * remove spaces and fix if condition
    
    ---------
    
    Co-authored-by: solloball <k.romanov1@g.nsu.ru>

--- include/collisions.hpp
+++ include/collisions.hpp
@@ -6,6 +6,7 @@
 bool CollidePrimitive(AABB, AABB);
 bool CollidePrimitive(Sphere, Sphere);
 bool CollidePrimitive(Triangle, Triangle);
+bool CollidePrimitive(OBB, OBB);
 
 bool CollidePrimitive(AABB, Triangle);
 bool CollidePrimitive(Triangle, AABB);
@@ -19,22 +20,44 @@ bool CollidePrimitive(Sphere, AABB);
 bool CollidePrimitive(Triangle, Sphere);
 bool CollidePrimitive(Sphere, Triangle);
 
+bool CollidePrimitive(Plane, OBB);
+bool CollidePrimitive(OBB, Plane);
+
+bool CollidePrimitive(Sphere, OBB);
+bool CollidePrimitive(OBB, Sphere);
+
+bool CollidePrimitive(Triangle, OBB);
+bool CollidePrimitive(OBB, Triangle);
+
+bool CollidePrimitive(AABB, OBB);
+bool CollidePrimitive(OBB, AABB);
+
 template<typename T>
 bool CollideMeshAt(T t, Mesh *mesh, Transform transform);
 bool CollideMeshes(Mesh *mesh, Transform transform, Mesh *mesh2, Transform transform2);
 
 bool CollidePrimitive(Ray, Sphere);
 bool CollidePrimitive(Ray, AABB);
+bool CollidePrimitive(Ray, OBB);
 
 std::optional<float> CollisionPrimitive(Ray, Sphere);
 std::optional<float> CollisionPrimitive(Ray, AABB);
+std::optional<float> CollisionPrimitive(Ray, OBB);
 
-Vec3 CollisionNormal(AABB, AABB,     Transform, Transform, Vec3, float);
-Vec3 CollisionNormal(Sphere, AABB,   Transform, Transform, Vec3, float);
-Vec3 CollisionNormal(AABB, Sphere,   Transform, Transform, Vec3, float);
-Vec3 CollisionNormal(AABB, Mesh*,    Transform, Transform, Vec3, float);
-Vec3 CollisionNormal(Mesh*, AABB,    Transform, Transform, Vec3, float);
+
+Vec3 CollisionNormal(AABB, AABB, Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(Sphere, AABB, Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(AABB, Sphere, Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(AABB, Mesh*, Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(Mesh*, AABB, Transform, Transform, Vec3, float);
 Vec3 CollisionNormal(Sphere, Sphere, Transform, Transform, Vec3, float);
-Vec3 CollisionNormal(Sphere, Mesh*,  Transform, Transform, Vec3, float);
-Vec3 CollisionNormal(Mesh*, Sphere,  Transform, Transform, Vec3, float);
-Vec3 CollisionNormal(Mesh*, Mesh*,  Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(Sphere, Mesh*, Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(Mesh*, Sphere, Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(Mesh*, Mesh*, Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(OBB, OBB, Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(AABB, OBB, Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(OBB, AABB, Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(Sphere, OBB, Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(OBB, Sphere, Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(OBB, Mesh*, Transform, Transform, Vec3, float);
+Vec3 CollisionNormal(Mesh*, OBB, Transform, Transform, Vec3, float);
