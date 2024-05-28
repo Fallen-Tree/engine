7129d66661e88e0c668386b43b8d4d69664e77e4
Egor <egorka99966@gmail.com>
2024-05-28 19:02:46 +0700

    Cleanup

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -65,40 +65,16 @@ void AddLantern(Vec3 pos) {
 }
 
 void createLights() {
-    /* engine->NewObject().AddPointLight( */
-    /*     Vec3(0.0f, 0.0f, 0.0f), Vec3(1.0f, 1.0f, 1.0f), */
-    /*     Vec3(1.0f, 1.0f, 1.0f), Vec3(0.0, 10.0, 0.0), */
-    /*     1.f, 0.0f, 0.01f); */
-
-    /* engine->NewObject().AddPointLight( */
-    /*     Vec3(0.0f, 0.0f, 0.0f), Vec3(1.0f, 1.0f, 1.0f), */
-    /*     Vec3(1.0f, 1.0f, 1.0f), Vec3(0.0f, 10.0f, -20.0f), */
-    /*     1.f, 0.0f, 0.01f); */
-
-    /* engine->NewObject().AddPointLight( */
-    /*     Vec3(0.0f, 0.0f, 0.0f), Vec3(1.0f, 1.0f, 1.0f), */
-    /*     Vec3(1.0f, 1.0f, 1.0f), Vec3(0.0f,  10.0f, 20.0f), */
-    /*     1.f, 0.0f, 0.01f); */
     engine->NewObject().AddDirLight(
         Vec3(0.01f), Vec3(0.3f),
         Vec3(0.3f),  Vec3(-0.2f, -1.0f, -0.3f));
 
-    /* engine->NewObject().AddDirLight( */
-    /*     Vec3(0.2f), Vec3(0.7f, 0.1f, 0.1f), */
-    /*     Vec3(0.7f, 0.1f, 0.1f),  Vec3(0.5f, -1.0f, 0.2f)); */
     auto spotLight = engine->NewObject();
     auto light = spotLight.AddSpotLight(
         Vec3(0.0f, 0.0f, 0.0f), Vec3(1.0f, 1.0f, 1.0f), Vec3(1.0f, 1.0f, 1.0f),
         0.9f, 0.08f, 0.022f,
         glm::cos(glm::radians(15.f)), glm::cos(glm::radians(25.0f)));
     spotLight.AddTransform(Vec3(0.f, 9.f, 0.f), Vec3(1.f), Mat4(1.f));
-    /*
-    engine->NewObject().AddSpotLight(Vec3(0.0f, 0.0f, 0.0f), Vec3(0.5f, 0.5f, 0.5f),
-        Vec3(0.3f, 0.3f, .3f), Vec3(1.0f, 1.0f, 1.0f),
-        1.0f, 0.09f, 0.032f, Vec3(0),
-        glm::cos(glm::radians(10.0f)),
-        glm::cos(glm::radians(20.0f)));
-    */
 }
 
 void buildRoom() {
